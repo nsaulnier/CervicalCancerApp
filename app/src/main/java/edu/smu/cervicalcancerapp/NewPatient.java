45f3b58b20ac935407fee13a8c3f705212532cca
nsaulnier <nsaulnier2011@sbcglobal.net>
2014-11-24 21:50:28 -0600

    begin update to newpatient

--- app/src/main/java/edu/smu/cervicalcancerapp/NewPatient.java
+++ app/src/main/java/edu/smu/cervicalcancerapp/NewPatient.java
@@ -1,22 +1,39 @@
 package edu.smu.cervicalcancerapp;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 
 public class NewPatient extends Activity {
+
+    Button createNewPatient;
+    EditText enterFirstName;
+    EditText enterLastName;
+    DatePicker enterDOB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_patient);
 
-        Button createNewPatient = (Button) findViewById(R.id.create_new_patient);
+        createNewPatient = (Button) findViewById(R.id.create_new_patient);
+        enterFirstName = (EditText) findViewById(R.id.enter_first_name);
+        enterLastName = (EditText) findViewById(R.id.enter_family_name);
+        enterDOB = (DatePicker) findViewById(R.id.enter_dob);
+
+        // come back to this: is there a way to make the keyboard go away when they tap outside?
+        //InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        //inputManager.hideSoftInputFromWindow(enterFirstName.getWindowToken(),0);
+        //inputManager.hideSoftInputFromWindow(enterLastName.getWindowToken(),0);
 
         //set the createNewPatient onClickListener
         View.OnClickListener createNewPatientHandler = new View.OnClickListener() {
